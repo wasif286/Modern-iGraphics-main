4935fbfdf30e8004dc47590bfd08ed5b91139eb9
wasif286 <mahmudulhasanwasif@gmail.com>
2025-07-08 19:31:27 +0600

    Week 11

--- iMain.cpp
+++ iMain.cpp
@@ -1,36 +1,127 @@
 #include "iGraphics.h"
 #include<unistd.h>
+#include<time.h>
 #include<math.h>
+#include <iostream>
+#include "iSound.h"
+using namespace std;
 #define SCREEN_HEIGHT 600
 #define SCREEN_WIDTH 1000
-double ball_x1=10,ball_y1=10,radius=15,dx=5,head_x=450,head_x2=550,head_y=120,head_y2=120,head_radius=25;
-int ball_x =500,ball_y=0,timer_start=0,ball_touched_ceil=0,ball_shoot=0,kick_off=0;
-double leg_top=30 ,leg_top2=30, leg_bottom=0, leg_bottom2=0;
-/*
-function iDraw() is called again and again by the system.
-*/
+int menuSoundPlayed = 0;
+int kickSoundChannel = -1;
+
+int scoreP1 = 0;
+int scoreP2 = 0;
+int goal=0;
+int goalanicount=0;
+int isShowingGoalAnimation = 0;
+// Game States:
+enum GameState { MENU, HELP, GAME };
+int currentState = MENU;
+int menuSelection = 0; // 0=Start, 1=Help, 2=Exit
+
+double ball_x1=10,ball_y1=10,radius=15,dx=5,head_x=450,head_x2=550,head_y=100,head_y2=100,head_radius=25;
+int ball_x =500,ball_y=200,timer_start=0,ball_touched_ceil=0,ball_shoot=0,kick_off=0,p2_jump=0,up=10,p1_jump=0,timer2=0;
+double leg_top=30 ,leg_top2=30, leg_bottom=0, leg_bottom2=0,dy=3,time_count=0;
+
+void jump()
+{ // when heading while jumping player should not go up to avoid multitple collisions
+   if(p2_jump==1)
+   {if ((head_y2 > 300) || 
+    ((((ball_x > head_x2 ? (ball_x - head_x2) : (head_x2 - ball_x)) <= 40) &&
+    (ball_y <= head_y2 + head_radius) && 
+    (ball_y >= leg_bottom2))))
+        up=-up; 
+    head_y2+=up;
+        leg_top2+=up;
+        leg_bottom2+=up;
+     if(head_y2 < 100) 
+           { head_y2 = 100;
+            leg_top2 = 30;
+            leg_bottom2 =0;
+            p2_jump =0;
+            up=-up;
+        }}
+    if(p1_jump==1)
+   {if ((head_y > 300) || 
+    ((((ball_x > head_x ? (ball_x - head_x) : (head_x - ball_x)) <= 40) &&
+    (ball_y <= head_y + head_radius) && 
+    (ball_y >= leg_bottom))))
+       up=-up; 
+     head_y+=up;
+        leg_top+=up;
+        leg_bottom+=up;
+     if(head_y < 100) 
+           { head_y = 100;
+            leg_top = 30;
+            leg_bottom =0;
+            p1_jump =0;
+        up=-up;}}
+           }
+
 void ball_move()
-{  
+{    if (isShowingGoalAnimation) return; 
+    if(kick_off ==0) {ball_x =500;
+                      if(ball_y >=200 || ball_y <=head_radius) dy=-dy;
+                       ball_y+=dy;
+                       // goal = 0;
+                        goalanicount= 0;}
     if(kick_off == 1) ball_x+=dx;
    // This is the parabolic eq of the ball when headed.
-   if(ball_touched_ceil == 0 && ball_shoot == 0 && kick_off ==1) ball_y= (ball_x-ball_x1-400)*(ball_x-ball_x1-400)/(-800) + ball_y1 + 200;
+   if(ball_touched_ceil == 0 && ball_shoot == 0 && kick_off ==1) ball_y= (ball_x-ball_x1-300)*(ball_x-ball_x1-300)/(-450) + ball_y1 + 200;
    else if(ball_touched_ceil ==1) ball_y-=10;
+   if((((ball_x > head_x)? (ball_x - head_x) : (head_x - ball_x) )<=40) && ball_y <= head_y + head_radius && ball_y >=leg_bottom)
+        {   
+            //iStopSound();
+               iPlaySound("assets/sounds/kick.wav", false,100);
+            if(ball_x > head_x) dx = (dx>0)?dx:-dx;
+            else dx = (dx>0)?-dx:dx;
+            kick_off =1;
+        if(dx>0) {ball_x1 = ball_x;
+         ball_y1 = ball_y;}
+        else {ball_x1 = ball_x-600;
+         ball_y1 = ball_y;
+        }
+     ball_touched_ceil=0;
+     ball_shoot =0;
+        }
+    else if((((ball_x > head_x2)? (ball_x - head_x2) : (head_x2 - ball_x) )<=40) && ball_y <= head_y2 + head_radius && ball_y >=leg_bottom2)
+        {
+            // iStopSound();
+               iPlaySound("assets/sounds/kick.wav", false,100);
+             if(ball_x > head_x2) dx = (dx>0)?dx:-dx;
+            else dx = (dx>0)?-dx:dx;
+            // iPlaySound("assets/sounds/kick.wav", false);
+            
+             kick_off =1;
+         if(dx<0){ball_x1 = ball_x-600;
+         ball_y1 = ball_y;}
+        else{
+            ball_x1 = ball_x;
+            ball_y1 = ball_y;
+        }
+     ball_touched_ceil=0;
+     ball_shoot =0;
+        }
    if(ball_x + radius>SCREEN_WIDTH ){
+    iPlaySound("assets/sounds/kick.wav", false,100);
      ball_x = SCREEN_WIDTH - radius;
-     ball_x1=ball_x-800;
+     ball_x1=ball_x-600;
      ball_y1 = ball_y;
      dx=-dx;
      ball_touched_ceil=0;
      ball_shoot=0;
    }
    if(ball_x > 950 && ball_y + radius > 150 && ball_y - radius < 150){
-     ball_x1=ball_x-800;
+    iPlaySound("assets/sounds/kick.wav", false,100);
+     ball_x1=ball_x-600;
      ball_y1 = ball_y;
      dx=-dx;
      ball_touched_ceil=0;
      ball_shoot=0; 
    }
    else if(ball_x < radius ){
+    iPlaySound("assets/sounds/kick.wav", false,100);
      ball_x = radius;
      ball_x1 = ball_x;
      ball_y1 = ball_y;
@@ -39,6 +130,7 @@ void ball_move()
      ball_shoot=0;
    }
    if(ball_x < 50 && ball_y + radius > 150 && ball_y - radius < 150){
+    iPlaySound("assets/sounds/kick.wav", false,100);
      ball_x1 = ball_x;
      ball_y1 = ball_y;
      dx=-dx;
@@ -46,21 +138,23 @@ void ball_move()
      ball_shoot=0;
    }
    if(ball_y + radius > SCREEN_HEIGHT){
+    iPlaySound("assets/sounds/kick.wav", false,100);
      ball_y = SCREEN_HEIGHT - radius;
-     if(dx<0) ball_x1 = ball_x-800;
+     if(dx<0) ball_x1 = ball_x-400;
      else ball_x1 = ball_x;
      ball_y1 = ball_y;
      ball_touched_ceil=1;
      ball_shoot = 0;
    }
    if(ball_y < radius){
+    iPlaySound("assets/sounds/kick.wav", false,100);
        if(dx>0){
         ball_y = radius;
         ball_x1 = ball_x;
         ball_y1 = ball_y;}
         else{
             ball_y = radius;
-            ball_x1 = ball_x -800;
+            ball_x1 = ball_x -600;
             ball_y1 = ball_y;
         }
     ball_touched_ceil=0;
@@ -68,15 +162,132 @@ void ball_move()
    }
     if((ball_x < 40 && ball_y + radius < 150) || (ball_x > 960 && ball_y + radius < 150))
     { 
-     sleep(1);
-     ball_touched_ceil =0;
-     ball_shoot =0;
-     ball_x = 500, ball_y =0;
-    kick_off =0;}
-   
+         iPlaySound("assets/sounds/goal.wav", false,100);
+        goal=1;
+         goalanicount = 100;
+             isShowingGoalAnimation = 1; 
+         
+        // iResumeTimer(1);  // Resume the goal animation timer
+
+        if(ball_x < 40 && ball_y + radius < 150) {
+            scoreP2++;
+        } else if (ball_x > 960 && ball_y + radius < 150) {
+            scoreP1++;
+        }
+     //sleep(1);
+    //  ball_touched_ceil =0;
+    //  ball_shoot =0;
+    //  ball_x = 500, ball_y =200;
+    // kick_off =0;
+    // dy=5;
+    // head_x = 
+    // p1_jump=0;
+    // p2_jump =0;
+    // up=10;
+    // head_y = 100;
+    // leg_top = 30;
+    // leg_bottom =0;
+    // head_y2 = 100;
+    // leg_top2 = 30;
+    // leg_bottom2 =0;
+     ball_x1=10,ball_y1=10,radius=15,dx=5,head_x=450,head_x2=550,head_y=100,head_y2=100,head_radius=25;
+ ball_x =500,ball_y=200,ball_touched_ceil=0,ball_shoot=0,kick_off=0,p2_jump=0,up=10,p1_jump=0;
+leg_top=30 ,leg_top2=30, leg_bottom=0, leg_bottom2=0,dy=3;
+
 }
-void iDraw()
-{   iClear();
+
+time_count +=.01;
+if(time_count>93.90){
+    iPauseTimer(0);
+    time_count=0;
+     scoreP1 = 0;
+scoreP2 = 0;
+ goal=0;
+ goalanicount=0;
+ isShowingGoalAnimation = 0;
+  ball_x1=10,ball_y1=10,radius=15,dx=5,head_x=450,head_x2=550,head_y=100,head_y2=100,head_radius=25;
+ ball_x =500,ball_y=200,ball_touched_ceil=0,ball_shoot=0,kick_off=0,p2_jump=0,up=10,p1_jump=0;
+leg_top=30 ,leg_top2=30, leg_bottom=0, leg_bottom2=0,dy=3;
+
+    currentState = MENU;
+
+
+}
+}
+   
+
+
+Image bg, goalim,ball,field;
+void loadResources()
+{
+	iLoadImage(&bg, "assets/images/footballbg.png");
+     iResizeImage(&bg, 1000, 600);
+     iLoadImage(&goalim, "assets/images/goal.png");
+     iResizeImage(&goalim, 1000, 600);
+     iLoadImage(&ball, "assets/images/SoccerBall.png");
+     iResizeImage(&ball, 30, 30);
+     iLoadImage(&field, "assets/images/field.png");
+     iResizeImage(&field, 1000, 600);
+
+       // resize to match screen
+	// iLoadImage(&mario, "assets/images/mario.png");
+	// double marioAspectRatio = (double)mario.width / mario.height;
+	// iResizeImage(&mario, 48, 48.0 / marioAspectRatio);
+}
+/*
+function iDraw() is called again and again by the system.
+*/
+void iDraw() {
+    iClear();
+    
+    if(currentState == MENU) {
+        if(!menuSoundPlayed) {
+            iPlaySound("assets/sounds/footballbg.wav", true,50); // true = loop
+            menuSoundPlayed = 1;
+        }
+
+        // your menu drawing code...
+
+     
+
+        
+        	iShowLoadedImage(0, 0, &bg);
+        iSetColor(255, 255, 255);
+iSetTransparentColor(255,255,255,0.2);
+    iFilledRectangle(445,245,100,28);
+    iFilledRectangle(445,295,100,28);
+     iFilledRectangle(445,345,100,28);
+       iSetColor(255, 255, 0);
+        iText(400, 550, "2 Player Football Game", GLUT_BITMAP_TIMES_ROMAN_24);
+        
+        if(menuSelection == 0) iSetColor(255, 0, 0); else iSetColor(255, 255, 255);
+        iText(450, 350, "Start Game", GLUT_BITMAP_HELVETICA_18);
+        
+        if(menuSelection == 1) iSetColor(255, 0, 0); else iSetColor(255, 255, 255);
+        iText(450, 300, "   Help   ", GLUT_BITMAP_HELVETICA_18);
+        
+        if(menuSelection == 2) iSetColor(255, 0, 0); else iSetColor(255, 255, 255);
+        iText(450, 250, "   Exit  ", GLUT_BITMAP_HELVETICA_18);
+    }
+    
+    else if(currentState == HELP) {
+        iSetColor(255, 255, 0);
+        iText(400, 500, "Help / Instructions", GLUT_BITMAP_TIMES_ROMAN_24);
+        iSetColor(255, 255, 255);
+        iText(200, 450, "Player 1 : WASD to move, S=Shoot, H=Header");
+        iText(200, 400, "Player 2 : Arrow Keys to move, K=Shoot, L=Header");
+        iText(200, 350, "First to score wins");
+        iText(400, 200, "Press 'b' to go back to Menu");
+    }
+    
+
+    
+    else if(currentState == GAME) {
+
+ 
+
+
+        
     iSetColor(135,206,235);
     iFilledRectangle(0,150,1000,450);
     iSetColor(154,236,153);
@@ -92,6 +303,8 @@ void iDraw()
     iSetColor(255,0,0);
     iFilledCircle(ball_x,ball_y,radius);
     // place your drawing codes here
+
+        	iShowLoadedImage(0,0, &field);
     iSetColor(0,0,0);
 // This is the head of player1
     iFilledCircle(head_x,head_y,head_radius);
@@ -103,6 +316,7 @@ void iDraw()
     iSetColor(0,0,255);
     // This is the head of player2
     iFilledCircle(head_x2,head_y2,head_radius);
+
 // This is the body 2
     iFilledRectangle(head_x2 - head_radius,leg_top2,2*head_radius,head_y2 - leg_top2 - head_radius);
 // Leg 2
@@ -110,8 +324,57 @@ void iDraw()
     iLine(head_x2,leg_top2,head_x2 + 20 ,leg_bottom2);
     iSetColor(255,0,0);
     iFilledCircle(ball_x,ball_y,radius);
+    iShowLoadedImage(ball_x-15, ball_y-15, &ball);
+    // Draw background bar
+    iSetColor(0, 0, 0);  // black background/
+    iFilledRectangle(390, 570, 230, 30);
+
+    // Set text color
+    iSetColor(255, 255, 255);  // white text
+
+    // Draw score texts
+    char scoreText1[20];
+    char scoreText2[20];
+    char time[50];
+    
+
+     sprintf(time,"   TIME: %0.2f",time_count);
+    sprintf(scoreText1, "Player 1: %d", scoreP1);
+    sprintf(scoreText2, "Player 2: %d", scoreP2);
+    
+
+    iText(420, 580, scoreText1, GLUT_BITMAP_HELVETICA_18);
+    iText(520, 580, scoreText2, GLUT_BITMAP_HELVETICA_18);
+    if(time_count<=90.00){iText(660,580,time,GLUT_BITMAP_HELVETICA_18);}
+
+    if(time_count>90.00){
+        iSetColor(0, 0, 0);  // black background/
+    iFilledRectangle(100, 30, 800, 540);
+     iSetColor(255, 255, 255);
+    if(scoreP1>scoreP2){iText(490, 320, "HURRAH", GLUT_BITMAP_HELVETICA_18);
+
+        iText(420, 290, "Player 1 has won the match", GLUT_BITMAP_HELVETICA_18);}
+       else if(scoreP1<scoreP2){iText(490, 320, "HURRAH", GLUT_BITMAP_HELVETICA_18);
+
+        iText(420, 290, "Player 2 has won the match", GLUT_BITMAP_HELVETICA_18);}
+        else {iText(425, 320, "YOU TWO PLAYED WELL", GLUT_BITMAP_HELVETICA_18);
+
+        iText(460, 290, "The match is draw", GLUT_BITMAP_HELVETICA_18);}
+
+    }
+
+if (goal) {
+  iShowLoadedImage(0, 0, &goalim);  
+  goalanicount--;
+  
+
+  if (goalanicount <=0) { 
+    goal = 0;   isShowingGoalAnimation = 0;
+     goalanicount = 0;}
+    
 }
 
+}}
 /*
 function iMouseMove() is called when the user moves the mouse.
 (mx, my) is the position where the mouse pointer is.
@@ -159,10 +422,32 @@ void iMouseWheel(int dir, int mx, int my)
 function iKeyboard() is called whenever the user hits a key in keyboard.
 key- holds the ASCII value of the key pressed.
 */
+
 void iKeyboard(unsigned char key)
 {
+if(currentState == MENU) {
+        if(key == '\r') { // ENTER pressed
+            iPlaySound("assets/sounds/press.wav", false,100);
+            if(menuSelection == 0) currentState = GAME;
+            if(menuSelection == 1) currentState = HELP;
+            if(menuSelection == 2) exit(0);
+        }
+    }
+    
+    else if(currentState == HELP) {
+        if(key == 'b') {iPlaySound("assets/sounds/press.wav", false,100);
+            iPauseTimer(0); currentState = MENU;}
+    }
+else if(currentState == GAME) {
+      
     switch (key)
     {
+        case 'b':
+{    iPauseTimer(0);  // pause ball movement timer
+    currentState = MENU;
+    break;}
+      
+
     case 'q':{
         // do something with 'q'
         iPauseTimer(0);
@@ -174,40 +459,17 @@ void iKeyboard(unsigned char key)
        
     case 's':{
         if((((ball_x > head_x)? (ball_x - head_x) : (head_x - ball_x) )<=40) && ball_y <= head_y + head_radius && ball_y >=leg_bottom)
-        {if(timer_start==0 ){
-                iSetTimer(10,ball_move);
-               timer_start =1;}
-        if(kick_off == 10) kick_off =1;
+        {
+               kick_off =1;
         ball_shoot =1, ball_touched_ceil=0;
         dx=(dx>0)?dx:-dx;
         }break;
     }
-    case '\r':{
-        if(kick_off ==0) {ball_x =500,ball_y =0;
-                         kick_off =10;}
-        break;
-    }
+
     case 'w':{
-        if(head_y <= SCREEN_HEIGHT - 30)
-       { head_y+=30;
-        leg_top+=30;
-        leg_bottom+=30;}
-        else{ head_y = SCREEN_HEIGHT - head_radius;
-        leg_top = head_y - 90;
-        leg_bottom = leg_top - 30; }
-        break;
-    }
-    case 'z':{
-        if(head_y >=150) {head_y-=30;
-        leg_top-=30;
-        leg_bottom-=30;}
-        else {
-            head_y = 120;
-            leg_top = 30;
-            leg_bottom =0;
-        }
-        break;
-    }
+       p1_jump =1;
+    break;}
+    
     case 'a' :{
         if(head_x>=30)
         head_x-=30;
@@ -215,52 +477,23 @@ void iKeyboard(unsigned char key)
         break;
     }
     case 'd':{
-         if(head_x <= SCREEN_WIDTH - 30)
+        if(head_x <= SCREEN_WIDTH - 30)
         head_x+=30;
         else head_x = SCREEN_WIDTH - head_radius;
         break;
     }
-    case 'h':{
-         if((((ball_x > head_x)? (ball_x - head_x) : (head_x - ball_x) )<=40) && ball_y <= head_y + head_radius && ball_y >=leg_bottom)
-        {
-            if(kick_off == 10) kick_off =1;
-            if(timer_start==0 ){
-                iSetTimer(10,ball_move);
-               timer_start =1;}
-        if(dx<0) dx =-dx;
-         ball_x1 = ball_x;
-         ball_y1 = ball_y;
-     ball_touched_ceil=0;
-     ball_shoot =0;
-        }
-        break;
-    }
-    case 'l':{
-        if((((ball_x > head_x2)? (ball_x - head_x2) : (head_x2 - ball_x) )<=40) && ball_y <= head_y2 + head_radius && ball_y >=leg_bottom2)
-        {
-            if(kick_off == 10) kick_off =1;
-            if(timer_start==0 ){
-                iSetTimer(10,ball_move);
-               timer_start =1;}
-        if(dx>0) dx =-dx;
-         ball_x1 = ball_x-800;
-         ball_y1 = ball_y;
-     ball_touched_ceil=0;
-     ball_shoot =0;
-        }
-        break;
-    }
+
+       
     case 'k':{
         if((((ball_x > head_x2)? (ball_x - head_x2) : (head_x2 - ball_x) )<=40) && ball_y <= head_y2 + head_radius && ball_y >=leg_bottom2)
-        {if(timer_start==0 ){
-                iSetTimer(10,ball_move);
-               timer_start =1;}
-        if(kick_off == 10) kick_off =1;
+        {
+         kick_off =1;
         ball_shoot =1,ball_touched_ceil =0;
         dx=(dx<0)?dx:-dx;
         }break;
     }
-    // place your codes for other keys here
+}
+   
 }
 }
 
@@ -275,6 +508,17 @@ GLUT_KEY_PAGE_UP, GLUT_KEY_PAGE_DOWN, GLUT_KEY_HOME, GLUT_KEY_END,
 GLUT_KEY_INSERT */
 void iSpecialKeyboard(unsigned char key)
 {
+     if(currentState == MENU) {
+        if(key == GLUT_KEY_DOWN) {
+            iPlaySound("assets/sounds/press.wav", false,100);
+            menuSelection = (menuSelection + 1) % 3;
+        }
+        if(key == GLUT_KEY_UP) {iPlaySound("assets/sounds/press.wav", false,100);
+            menuSelection = (menuSelection - 1 + 3) % 3;
+        }
+    }
+
+    if(currentState == GAME) {
     switch (key)
     {
     case GLUT_KEY_END:
@@ -282,25 +526,10 @@ void iSpecialKeyboard(unsigned char key)
         break;
     // place your codes for other keys here
     case GLUT_KEY_UP:{
-     if(head_y2 <= SCREEN_HEIGHT - 30)
-       { head_y2+=30;
-        leg_top2+=30;
-        leg_bottom2+=30;}
-        else{ head_y2 = SCREEN_HEIGHT - head_radius;
-        leg_top2 = head_y2 - 90;
-        leg_bottom2 = leg_top2 - 30; }
-        break;}
-    case GLUT_KEY_DOWN:{
-       if(head_y2 >=150) {head_y2-=30;
-        leg_top2-=30;
-        leg_bottom2-=30;}
-        else {
-            head_y2 = 120;
-            leg_top2 = 30;
-            leg_bottom2 =0;
-        }
-        break;}
+     p2_jump =1;
+    break;}
     case GLUT_KEY_LEFT:{
+        // collision between players
       if(head_x2>=30)
         head_x2-=30;
         else head_x2 = 25;
@@ -309,16 +538,28 @@ void iSpecialKeyboard(unsigned char key)
         if(head_x2 <= SCREEN_WIDTH - 30)
         head_x2+=30;
         else head_x2 = SCREEN_WIDTH - head_radius;
-        break;}
+        break;
+    }
     default:
         break;
     }
-}
 
-int main(int argc, char *argv[])
-{
+}
+       
+    }
+int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
-    // place your own initialization codes here.
-    iInitialize(SCREEN_WIDTH,SCREEN_HEIGHT, "Simple Soccer");
+    loadResources();
+     iInitializeSound();
+    //iSetTimer(20, goalAnimationUpdate);  // Timer 1 for animation
+   //  iPauseTimer(1);
+     if(timer_start==0 ){
+                iSetTimer(10,ball_move);
+               timer_start =1;}
+    if(timer2==0) {iSetTimer(10,jump);
+                   timer2 =1;}
+
+    iInitialize(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Soccer");
     return 0;
 }
+
